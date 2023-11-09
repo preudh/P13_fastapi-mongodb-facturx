764c1c77fd41757fa6d73d44a1a9daee0697e12c
Patrice Preudhomme <preudhomme.patrice@gmail.com>
2023-11-09 16:03:08 +0100

    gitignore

--- database.py
+++ database.py
@@ -1,10 +1,4 @@
-# import motor.motor_asyncio  # USE THIS CODE TO CONNECT TO YOUR LOCAL MONGODB
-#
-# # Used to connect to the database
-# client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')  # Create a connection to the DB_NAME
-# database = client.local  # local is the name of the database in MongoDB
-# collection = database.invoice  # invoice is the name of the collection in MongoDB://localhost:27017/")
-
+# Description: This file contains the code to connect to the MongoDB database.
 import motor.motor_asyncio
 import os
 
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-# Import statements
-from typing import Any, Callable, Set, TypeVar
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 from crud import invoice_router
 
 # Create app object
 app = FastAPI()
 
-# register your router
+# Redirect root to /docs
+@app.get("/", include_in_schema=False) # include_in_schema=False to hide the route from the documentation
+async def redirect_to_docs():
+    return RedirectResponse(url='/docs') # redirect to /docs route instead of /
+
+# Register your router
 app.include_router(invoice_router)
